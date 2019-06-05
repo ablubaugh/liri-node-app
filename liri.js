f9564100391d3bf8cbc0d1863905953ac1fcbeea
Abby Blubaugh <abigailrblubaugh@gmail.com>
2019-06-04 20:00:11 -0700

    Update liri.js
    required packages

--- liri.js
+++ liri.js
@@ -1,4 +1,10 @@
-require(".env").configt();
-
+///Require all packages 
+require('dotenv').config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
+///Spotify Search 
+
